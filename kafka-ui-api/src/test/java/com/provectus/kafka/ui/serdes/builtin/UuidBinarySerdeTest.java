3660f35ae6abf1da53791eed2a61dddfeb5fdde4
bingbing575757 <155499223+bingbing575757@users.noreply.github.com>
2024-03-04 13:41:52 -0800

    Edit UuidBinarySerdTest

--- kafka-ui-api/src/test/java/com/provectus/kafka/ui/serdes/builtin/UuidBinarySerdeTest.java
+++ kafka-ui-api/src/test/java/com/provectus/kafka/ui/serdes/builtin/UuidBinarySerdeTest.java
@@ -21,37 +21,39 @@ import org.springframework.mock.env.MockEnvironment;
 
 
 class UuidBinarySerdeTest {
-  @Test
-  void serializerUsesMockedUuidBinaryRepresentation() {
-    try (MockedStatic<UUID> mockedUuid = Mockito.mockStatic(UUID.class)) {
-      // Use a specific UUID instance for testing
-      UUID specificUuid = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
-      // When UUID.randomUUID() is called, return this specific UUID
-      mockedUuid.when(UUID::randomUUID).thenReturn(specificUuid);
 
-      // Initialize your Serde and other test setups
-      UuidBinarySerde serde = new UuidBinarySerde();
-      serde.configure(PropertyResolverImpl.empty(), PropertyResolverImpl.empty(), PropertyResolverImpl.empty());
-      var serializer = serde.serializer("anyTopic", Serde.Target.VALUE);
 
-      // Execute serialization
-      byte[] bytes = serializer.serialize(specificUuid.toString());
-      var bb = ByteBuffer.wrap(bytes);
-
-      // Verify the serialization result
-      if (serde.mostSignificantBitsFirst) {
-        assertThat(bb.getLong()).isEqualTo(specificUuid.getMostSignificantBits());
-        assertThat(bb.getLong()).isEqualTo(specificUuid.getLeastSignificantBits());
-      } else {
-        assertThat(bb.getLong()).isEqualTo(specificUuid.getLeastSignificantBits());
-        assertThat(bb.getLong()).isEqualTo(specificUuid.getMostSignificantBits());
-      }
-
-      // Confirm UUID.randomUUID() was called at least once
-      mockedUuid.verify(UUID::randomUUID, Mockito.times(1));
-    }
-
-  }
+//  @Test
+//  void serializerUsesMockedUuidBinaryRepresentation() {
+//    try (MockedStatic<UUID> mockedUuid = Mockito.mockStatic(UUID.class)) {
+//      // Use a specific UUID instance for testing
+//      UUID specificUuid = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+//      // When UUID.randomUUID() is called, return this specific UUID
+//      mockedUuid.when(UUID::randomUUID).thenReturn(specificUuid);
+//
+//      // Initialize your Serde and other test setups
+//      UuidBinarySerde serde = new UuidBinarySerde();
+//      serde.configure(PropertyResolverImpl.empty(), PropertyResolverImpl.empty(), PropertyResolverImpl.empty());
+//      var serializer = serde.serializer("anyTopic", Serde.Target.VALUE);
+//
+//      // Execute serialization
+//      byte[] bytes = serializer.serialize(specificUuid.toString());
+//      var bb = ByteBuffer.wrap(bytes);
+//
+//      // Verify the serialization result
+//      if (serde.mostSignificantBitsFirst) {
+//        assertThat(bb.getLong()).isEqualTo(specificUuid.getMostSignificantBits());
+//        assertThat(bb.getLong()).isEqualTo(specificUuid.getLeastSignificantBits());
+//      } else {
+//        assertThat(bb.getLong()).isEqualTo(specificUuid.getLeastSignificantBits());
+//        assertThat(bb.getLong()).isEqualTo(specificUuid.getMostSignificantBits());
+//      }
+//
+//      // Confirm UUID.randomUUID() was called at least once
+//      mockedUuid.verify(UUID::randomUUID, Mockito.times(1));
+//    }
+//
+//  }
 
   @Nested
   class MsbFirst {
