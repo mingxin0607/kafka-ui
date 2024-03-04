4fbebb141747b5e3156fca8ae807cf4a8c879b1c
bingbing575757 <155499223+bingbing575757@users.noreply.github.com>
2024-03-04 11:30:37 -0800

    Edit UuidTest

--- kafka-ui-api/src/test/java/com/provectus/kafka/ui/serdes/builtin/UuidBinarySerdeTest.java
+++ kafka-ui-api/src/test/java/com/provectus/kafka/ui/serdes/builtin/UuidBinarySerdeTest.java
@@ -50,28 +50,7 @@ class UuidBinarySerdeTest {
       // Confirm UUID.randomUUID() was called at least once
       mockedUuid.verify(UUID::randomUUID, Mockito.times(1));
     }
-//    try (MockedStatic<UUID> mockedUuid = Mockito.mockStatic(UUID.class)) {
-//      // 创建一个假的UUID实例
-//      UUID fakeUuid = new UUID(0L, 0L);
-//      // 当调用UUID.randomUUID()时，返回假的UUID
-//      mockedUuid.when(UUID::randomUUID).thenReturn(fakeUuid);
-//
-//      // 这里初始化你的Serde和其他测试设置
-//      UuidBinarySerde serde = new UuidBinarySerde();
-//      serde.configure(PropertyResolverImpl.empty(), PropertyResolverImpl.empty(), PropertyResolverImpl.empty());
-//      var serializer = serde.serializer("anyTopic", Serde.Target.VALUE);
-//
-//      // 执行序列化
-//      byte[] bytes = serializer.serialize(fakeUuid.toString());
-//      var bb = ByteBuffer.wrap(bytes);
-//
-//      // 验证序列化结果
-//      assertThat(bb.getLong()).isEqualTo(fakeUuid.getMostSignificantBits());
-//      assertThat(bb.getLong()).isEqualTo(fakeUuid.getLeastSignificantBits());
-//
-//      // 确认UUID.randomUUID()至少被调用了一次
-//      mockedUuid.verify(UUID::randomUUID, Mockito.times(1));
-//    }
+
   }
 
   @Nested
